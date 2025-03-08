722ec11abc6afb179c13fe413c29e62b0bb8413e
razanz <zbibrazan147@gmail.com>
2025-03-09 00:55:49 +0200

    add createQuestion.php

---
+++ article-server/api/createQuestion.php
@@ -0,0 +1,25 @@
+<?php
+require_once("../connection/connection.php");
+require_once("../models/Question.php");
+
+if (empty($_POST['question']) || empty($_POST['answer'])) {
+    echo json_encode([
+        "status" => "errorr",
+        "message" => "Please fill in all fields"
+    ]);
+    exit();
+}
+
+$question = $_POST['question'];
+$answer = $_POST['answer'];
+if (Question::createQuestion($conn, $question, $answer)) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Your FAQ created successfully"
+    ]);
+} else {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Failed to create ur FAQ"
+    ]);
+}
