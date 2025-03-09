99b4b2b25b39ca8087ea7fe370972a3b8c8ea43f
razanz <zbibrazan147@gmail.com>
2025-03-09 20:32:22 +0200

    add addQA.js

---
+++ article-client/js/addQA.js
@@ -0,0 +1,26 @@
+const questionInput = document.getElementById("Question");
+const answerInput = document.getElementById("Answer");
+const submitQABtn = document.getElementById("SubmitQABnt");
+const cancelBtn = document.getElementById("CancelBtn");
+
+cancelBtn.addEventListener('click', async () => {
+    window.location.href = "/article-client/home.html";
+})
+
+submitQABtn.addEventListener('click', async () => {
+    const form = new FormData();
+    form.append("question", questionInput.value);
+    form.append("answer", answerInput.value);
+
+    const response = await axios.post(
+        "http://localhost/all-bout-SDLC-article/article-server/api/createQuestion.php",
+        form
+    );
+
+    if (response) {
+        answerInput.value = "";
+        questionInput.value = "";
+        alert(response.data.message);
+    }
+
+});
